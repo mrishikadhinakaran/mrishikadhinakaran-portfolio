f8887849c0de891f14c563f877fad825d015f2b3
Raghav Katta <Raghavkatta2005@gmail.com>
2025-02-12 09:33:51 +0530

    Made Hero Component Responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,16 +4,16 @@ import Pfp from "../assets/Pfp.jpg"
 function Hero() {
 
     return (
-        <div className="flex flex-col lg:flex-row items-center justify-center  h-screen ">
+        <div className="flex flex-col lg:flex-row items-center justify-center  h-screen px-6 lg:px-20 text-center lg:text-left ">
 
-            <div className="flex flex-col">
+            <div className="flex flex-col items-center lg:items-start gap-2">
                 <p className="text-white text-3xl md:text-2xl animate-pulse">A MERN Stack developer</p>
-                <h1 className="text-9xl text-orange-500 font-bold ">Hello!,I'm <br />
+                <h1 className="text-5xl sm:text-7xl lg:text-9xl text-orange-500 font-bold ">Hello!,I'm <br />
                     Raghav Katta</h1>
 
             </div>
 
-            <img src={Pfp} alt="Profile Pic" className="w-[30rem] h-[30rem] rounded-full ml-[7.5rem]" />
+            <img src={Pfp} alt="Profile Pic" className="w-60 sm:w-100 md:w-110 lg:w-[30rem] items-center mt-[2rem]  rounded-full ml-[7.5rem] mb-6 lg:mb-0" />
 
 
 
