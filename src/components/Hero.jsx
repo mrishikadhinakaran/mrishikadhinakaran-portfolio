f27cada1e5226caef0be6a06af27481846a3179b
Raghav Katta <Raghavkatta2005@gmail.com>
2025-02-16 23:38:00 +0530

    Hero section responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import { motion } from "motion/react"
 import Pfp from "../assets/Pfp.jpg"
 function Hero() {
     return (
-        <div className="relative flex flex-col lg:flex-row items-center justify-center h-screen px-6 lg:px-20 text-center lg:text-left overflow-hidden">
+        <div className="relative flex flex-col lg:flex-row items-center justify-center h-screen px-6 lg:px-20 text-center lg:text-left overflow-hidden gap-20">
             {/* Background Animation */}
             <motion.div
                 className="absolute inset-0 opacity-20"
@@ -43,14 +43,14 @@ function Hero() {
                     </h1>
                 </motion.div>
             </div>
-<div className="">
+<div className="flex">
 <motion.img
                 src={Pfp}
                 alt="Profile Pic"
                 initial={{ opacity: 0, scale: 0.5 }}
                 animate={{ opacity: 1, scale: 1 }}
                 transition={{ duration: 0.8, delay: 0.4 }}
-                className="w-60 sm:w-100 md:w-110 sm:items-center lg:w-[30rem] mt-[2rem] rounded-full ml-[7.5rem] mb-6 lg:mb-0 hover:shadow-2xl hover:shadow-orange-500/50 transition-shadow duration-300 "
+                className="w-60 sm:w-100 md:w-110 sm:items-center lg:w-[30rem] mt-[2rem] rounded-full lg: mb-6 lg:mb-0 hover:shadow-2xl hover:shadow-orange-500/50 transition-shadow duration-300 "
                 whileHover={{ scale: 1.05 }}
                 whileTap={{ scale: 0.95 }}
             />
